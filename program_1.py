c0ccd8a1e3a9e268b7e33c2cecf4c6d5bc5b78f7
sl14-828 <samlee8179@gmail.com>
2024-09-12 13:16:11 -0500

    program_1.py
    Added my program from PyCharm

--- program_1.py
+++ program_1.py
@@ -1,6 +1,18 @@
 def personal_information():
-    print('...')
+    #Samuel Lee - Sept 9th
+    #program that displays my information
 
+    # #lines variables that store my information
+    myName = 'Samuel Lee'
+    myAddress = '19151 Iteri Ave, Lakville, MN, 55044'
+    myTelephoneNumber = '952-457-5531'
+    myCollegeMajor = 'Undecided (PSEO Student)'
 
+    #display information
+    print('My name is', myName, '!' )
+    print('My address is', myAddress, '!')
+    print('My telephone number is', myTelephoneNumber, '!')
+    print('My college is', myCollegeMajor, '!')
+    
 # Line which calls the above function.
 personal_information()
