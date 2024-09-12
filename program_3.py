eefa5ed79a53b6852a1dc3e5d6e519f64ca7ad55
sl14-828 <samlee8179@gmail.com>
2024-09-12 13:58:09 -0500

    program_3.py
    Imported my program from PyCharm

--- program_3.py
+++ program_3.py
@@ -1,8 +1,35 @@
+#Samuel Lee - Sept 12th
+#this program asks for 5 price values of 5 items, displays the subtotal, the amount of sales tax, and the total.
+
 def calculate_total_purchase():
-    # A customer in a store is purchasing five items.  
-    # Write a program that asks for each item, 
-    # then displays the subtotal of the sale, 
-    # the amount of sales tax, and the total.  
-    # Assume the sales tax is 7 percent.
+
+#prompt
+    print('This program asks for 5 price values of 5 items, '
+      'displays the subtotal, the amount of sales tax, '
+      'and the total.')
+
+#intializers
+    counter = 0
+    subtotal = 0
+    tax = .07
+    taxpercent = tax * 100
+
+#user input with while loop counter
+    print('(Please use 00.00 format! (example: 15.99)')
+    while counter < 5:
+        singleItemValue = float(input('Please Enter a Price: $'))
+
+        #Calculations of totalAge
+        subtotal += singleItemValue
+        counter += 1
+
+#Calculate Total After Tax and Display
+    total = subtotal * (tax + 1)
+
+    print('Subtotal: $', (round(subtotal, 2)),
+      '\nSales Tax: ', (round(taxpercent, 2)) ,'%'
+      '\nTotal: $', (round(total, 2)))
+
+
 
 calculate_total_purchase()
